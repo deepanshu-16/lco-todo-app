d317928213acb1402f4100687aead565799b6c9c
Deepanshu Narang <dn1832153@gmail.com>
2020-04-05 14:56:51 +0530

    Updates

---
+++ precache-manifest.04327d1dc72509ab21e362633bd12827.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cfae28b4c2b5bdfe0fb2a6b0487afbc",
+    "url": "/lco-todo-app/index.html"
+  },
+  {
+    "revision": "d6f231b2feb04a581bb2",
+    "url": "/lco-todo-app/static/css/main.1231689e.chunk.css"
+  },
+  {
+    "revision": "3e15c36a5b7297a4f154",
+    "url": "/lco-todo-app/static/js/2.f9bb857a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lco-todo-app/static/js/2.f9bb857a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f231b2feb04a581bb2",
+    "url": "/lco-todo-app/static/js/main.e78e489a.chunk.js"
+  },
+  {
+    "revision": "82bebede79190c64c55d",
+    "url": "/lco-todo-app/static/js/runtime-main.a4bb81ee.js"
+  },
+  {
+    "revision": "93326a916609381db6a9b85ae83f3f7f",
+    "url": "/lco-todo-app/static/media/LCO-logo-white.93326a91.png"
+  }
+]);
