773e9bd37509e7b6232928d95d1f1138f5a2b3cc
Deepanshu Narang <dn1832153@gmail.com>
2020-04-05 13:30:24 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <img
+          src={logo}
+          alt="react logo"
+          width="100"
+          height="100"
+          className="logo"
+        />
+        <h1 className="app-title">LCO ToDo App</h1>
+        <div className="container">
+          Add an Item....
+          <br />
+          <input
+            type="text"
+            className="input-text"
+            placeholder="write a todo"
+          />
+          <button className="add-btn">Add Todo</button>
+          <div className="list">
+            <ul>
+              <li>
+                <input type="checkbox" />
+                record youtube videos
+                <button className="btn">Delete</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
